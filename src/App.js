9015296549023c3f4469491d14484586523fd160
Chris <chris.eddy.dev@gmail.com>
2020-06-11 02:05:24 -0700

    update demo to work with square component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import Square from 'plant-fiber-ui';
+import React, { useRef, useState  } from 'react'
+import { Square } from 'plant-fiber-ui';
+import { Canvas, useFrame } from 'react-three-fiber'
 
 const App = (props) => {
   return (
-    <div className="App">
-      <Square />
-    </div>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Square position={[-1.2, 0, 0]} useRef={useRef} useState={useState} useFrame={useFrame} />
+      <Square position={[1.2, 0, 0]} useRef={useRef} useState={useState} useFrame={useFrame} />
+    </Canvas>
   );
 }
 
