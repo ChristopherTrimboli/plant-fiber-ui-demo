31dc6623d7ea09b8312b1fee9b8124a8ed881b65
Chris <chris.eddy.dev@gmail.com>
2020-06-10 23:32:58 -0700

    add react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Square from 'plant-fiber-ui';
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <Square />
+    </div>
+  );
+}
+
+export default App;
