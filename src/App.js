d9bb3cf2d6ad0df814e1fad47f12ced1d47fe5b5
Chris <chris.eddy.dev@gmail.com>
2020-06-12 19:35:31 -0700

    0.0.16

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Box } from 'plant-fiber-ui';
-import { VRCanvas, DefaultXRControllers } from 'react-xr' 
+import { Canvas, Box } from 'plant-fiber-ui';
+import { DefaultXRControllers } from 'react-xr' 
 
 const App = (props) => {
   return (
-    <VRCanvas>
+    <Canvas webxr>
       <DefaultXRControllers />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
-    </VRCanvas>
+    </Canvas>
   );
 }
 
