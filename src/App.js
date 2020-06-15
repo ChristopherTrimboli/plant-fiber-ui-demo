9651dca0ea406b98d8c41fc4f0d7aa64e3438b81
Chris <chris.eddy.dev@gmail.com>
2020-06-14 17:04:53 -0700

    0.0.21 - onAxesChange, localhost HTTPS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Canvas, Box } from 'plant-fiber-ui';
-import { DefaultXRControllers } from 'react-xr' 
+import React from 'react';
+import { Canvas, XRControllers, Box } from 'plant-fiber-ui';
 
 const App = (props) => {
+
+  const handleAxesChange = (axes, hand) => {
+    console.log(axes, hand)
+  }
+
   return (
-    <Canvas webxr>
-      <DefaultXRControllers />
+    <Canvas camera={{ fov: 75, position: [0, 0, 70] }} webxr>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
+      <XRControllers 
+        onSqueeze={(e) => console.log(e)} 
+        onSelect={(e) => console.log(e)} 
+        onSelectStart={(e) => console.log(e)} 
+        onAxesChange={handleAxesChange}
+      />
     </Canvas>
   );
 }
